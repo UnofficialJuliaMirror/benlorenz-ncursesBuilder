b8bc8e44949313da15e19ef5502a3b31d2ac28dd
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2019-09-20 13:18:39 +0200

    fix travis

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -11,20 +11,25 @@ sources = [
 script = raw"""
 # tic for host
 cd ${WORKSPACE}/srcdir/ncurses-*
-./configure CC="$CC_FOR_BUILD" CXX="$CXX_FOR_BUILD" RANLIB=$RANLIB_FOR_BUILD LD=$LD_FOR_BUILD CFLAGS="" LDFLAGS="" --without-shared --with-normal
+./configure CC="$CC_FOR_BUILD" CXX="$CXX_FOR_BUILD" RANLIB=$RANLIB_FOR_BUILD LD=$LD_FOR_BUILD CFLAGS="" LDFLAGS="" --without-shared --with-normal --disable-mixed-case
 make -C include
 make -C progs tic
 cp progs/tic /usr/bin
 
 make distclean
-./configure --prefix=${prefix} --host=${target} --build=x86_64-linux-gnu --with-shared --without-progs --with-build-cc="$CC_FOR_BUILD" --with-termlib --without-progs --without-tack --without-tests --without-manpages --without-normal
+./configure --prefix=${prefix} --host=${target} --build=x86_64-linux-gnu --with-shared --without-progs --with-build-cc="$CC_FOR_BUILD" --with-termlib --without-progs --without-tack --without-tests --without-manpages --without-normal --disable-mixed-case
 make -j${nproc}
 make install
+
+# avoid case-insensitive file system problems...
+rm -f $prefix/share/terminfo/*/hp70092A
+rm -f $prefix/share/terminfo/*/2621a
+rm -f $prefix/share/terminfo/*/hp2621a
 """
 
 products(prefix) = [
     LibraryProduct(prefix, "libncurses", :libncurses),
-    LibraryProduct(prefix, "libtermcap", :libtermcap)
+    LibraryProduct(prefix, "libtinfo", :libtinfo)
 ]
 
 platforms = filter(x->!isa(x,Windows),supported_platforms())
